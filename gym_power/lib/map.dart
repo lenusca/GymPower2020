76cc7282dda1b5ecc4b830b09502cb8c7e0ca26f
lenusca <helenamncardoso@gmail.com>
2020-03-08 18:06:10 +0000

    FInal version of MAP

--- gym_power/lib/map.dart
+++ gym_power/lib/map.dart
@@ -1,22 +1,13 @@
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:gym_power/home.dart';
-import 'package:path/path.dart' as Path;
 import 'package:gym_power/loading.dart';
 import 'package:gym_power/models/user.dart';
 import 'package:gym_power/service/database.dart';
 import 'package:gym_power/sidebar.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/services.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-
-
 
 class gpsMap extends StatefulWidget{
   static String tag = "map";
@@ -29,6 +20,7 @@ class gpsMapState extends State<gpsMap> {
   var currentLocation;
   var gyms = [];
   GoogleMapController mapController;
+  PageController _pageController;
   List<Marker> allmarkers = [];
 
   @override
@@ -42,6 +34,7 @@ class gpsMapState extends State<gpsMap> {
         localizationGyms();
       });
     });
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8);
   }
 
   // ir buscar a base dados todas as localizações
@@ -59,13 +52,102 @@ class gpsMapState extends State<gpsMap> {
   }
 
   initMarker(gyms) {
-    print(gyms['Name']);
-    print(gyms['Localization'].latitude);
-
     allmarkers.add(Marker(markerId: MarkerId(gyms['Name']),
         position: LatLng(
             gyms['Localization'].latitude, gyms['Localization'].longitude),
+        infoWindow: InfoWindow(title: gyms['Name'], snippet: 'CLOSED: '+gyms['Fechado']),
         draggable: false));
+  }
+
+  moveCamera(){
+    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(gyms[_pageController.page.toInt()]['Localization'].latitude, gyms[_pageController.page.toInt()]['Localization'].longitude),
+        zoom: 14.0,
+        bearing: 45.0,
+        tilt: 45.0
+    )));
+  }
+  _gymsList(index){
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget){
+        double value = 1;
+        if(_pageController.position.haveDimensions){
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3)+0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value)*130.0,
+            width: Curves.easeInOut.transform(value)*900.0,
+            child: widget,
+          ),
+        );
+      },
+      child: InkWell(
+        onTap: (){
+          moveCamera();
+        },
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+                height: 125.0,
+                width: 275.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(0.0, 4.0),
+                      blurRadius: 10.0
+                    )
+                  ]
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 90.0,
+                        width: 90.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10.0),
+                            topLeft: Radius.circular(10.0)
+                          ),
+                          image: DecorationImage(image: NetworkImage(
+                              gyms[index]['Image']
+                          ), fit: BoxFit.cover,  )
+                        ),
+                      ),
+                      SizedBox(width: 5.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(gyms[index]['Name'], style: TextStyle(color: Colors.deepOrangeAccent[200], fontSize: 12.0, fontWeight: FontWeight.bold),),
+                          Text('CLOSED ' +gyms[index]['Fechado'], style: TextStyle(color: Colors.red, fontSize: 9.0, fontWeight: FontWeight.w400),),
+                          Container(
+                            width: 160.0,
+                            child:  Text(gyms[index]['Descricao'], style: TextStyle(color: Colors.grey, fontSize: 10.0, fontWeight: FontWeight.w300),),
+                          ),
+
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -110,7 +192,7 @@ class gpsMapState extends State<gpsMap> {
                             .of(context)
                             .size
                             .height - 80.0,
-                        width: double.infinity,
+                        width: MediaQuery.of(context).size.width,
                         child: mapToggle ? GoogleMap(
                           onMapCreated: onMapCreated,
                           // posição inicial a do telemovel
@@ -121,6 +203,20 @@ class gpsMapState extends State<gpsMap> {
                         ) : Center(child: Text("Loading...", style: TextStyle(
                             color: Colors.deepOrangeAccent[200],
                             fontSize: 20.0),),),
+                      ),
+                      Positioned(
+                        bottom: 20.0,
+                        child: Container(
+                          height: 200.0,
+                          width: MediaQuery.of(context).size.width,
+                          child: PageView.builder(
+                              controller: _pageController,
+                              itemCount: gyms.length,
+                              itemBuilder: (BuildContext context, int index){
+                                return _gymsList(index);
+                              },
+                          ),
+                        ),
                       ),
 
                     ],
