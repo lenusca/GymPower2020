2d2f7bf46d196fe307bf51e683719681417130f5
anamiz <ana.maia97@hotmail.com>
2020-04-12 20:13:14 +0100

    my activities done

--- gym_power/lib/myActivities.dart
+++ gym_power/lib/myActivities.dart
@@ -25,15 +25,86 @@ class MyActivities extends StatefulWidget{
 
 class MyActivitiesState extends State<MyActivities>{
 
-  final List<Card> cardList = [];
-
+  final List<Card> aulasFreqCards = [];
+  final List<Card> workoutCards = [];
   
   fillCardList(data) async{
-    cardList.clear();
+    aulasFreqCards.clear();
+    workoutCards.clear();
 
+    //Fill workout
+    for(int i =0; i < data['workout'].length; i++)
+    {
+      workoutCards.add(Card(
+         semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(10),
+        child: Container(
+          height: 130,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+                           Row(
+                children: <Widget>[
+                 Container(
+                    width: 200,
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child:  Text("Workout", style: TextStyle(color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w500), textAlign: TextAlign.left, ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    child: Text("Duration", style: TextStyle(color: Colors.black,fontWeight: FontWeight.w500)),
+                  ),
+                 Container(
+                    margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    child: Text("Intensity", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500)),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child:  Text(data['workout'][i]["name"], style: TextStyle(color: Colors.deepOrangeAccent[200], fontSize: 20.0, fontWeight: FontWeight.w500), textAlign: TextAlign.left, ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    color: Colors.white,
+                    child: Text(data['workout'][i]["duration"], style: TextStyle(color: Colors.deepOrangeAccent[200], fontWeight: FontWeight.w500,fontSize: 20.0),),
+                  ),
+                 Container(
+                    margin: EdgeInsets.fromLTRB(50, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    color: Colors.white,
+                    child: Text(data['workout'][i]["intensity"], style: TextStyle(color: Colors.deepOrangeAccent[200], fontWeight: FontWeight.w500,fontSize: 20.0),),
+                  ),
+                ],
+              ),
+
+
+            ],
+          ),
+        ),
+      ));
+    }
+
+
+    //Fill aulas freq
     for(int i =0; i < data['aulasFrequentadas'].length; i++)
     {
-      cardList.add(Card(
+      aulasFreqCards.add(Card(
          semanticContainer: true,
         clipBehavior: Clip.antiAliasWithSaveLayer,
         shape: RoundedRectangleBorder(
@@ -50,6 +121,26 @@ class MyActivitiesState extends State<MyActivities>{
             children: <Widget>[
               Row(
                 children: <Widget>[
+                 Container(
+                    width: 200,
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child:  Text("Class Name", style: TextStyle(color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w500), textAlign: TextAlign.left, ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    child: Text("Start", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500)),
+                  ),
+                 Container(
+                    margin: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                    alignment: Alignment.centerRight,
+                    child: Text("Finish", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500)),
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
                   Container(
                     width: 200,
                     margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
@@ -57,19 +148,20 @@ class MyActivitiesState extends State<MyActivities>{
                   ),
 
                   Container(
-                    margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
                     alignment: Alignment.centerRight,
                     color: Colors.white,
-                    child: Text(data['aulasFrequentadas'][i]["horaInicio"], style: TextStyle(color: Colors.deepOrangeAccent[200]),),
+                    child: Text(data['aulasFrequentadas'][i]["horaInicio"], style: TextStyle(color: Colors.deepOrangeAccent[200],fontSize: 20.0, fontWeight: FontWeight.w500),),
                   ),
                  Container(
-                    margin: EdgeInsets.fromLTRB(70, 0, 0, 0),
+                    margin: EdgeInsets.fromLTRB(40, 0, 0, 0),
                     alignment: Alignment.centerRight,
                     color: Colors.white,
-                    child: Text(data['aulasFrequentadas'][i]["horaFim"], style: TextStyle(color: Colors.deepOrangeAccent[200]),),
+                    child: Text(data['aulasFrequentadas'][i]["horaFim"], style: TextStyle(color: Colors.deepOrangeAccent[200], fontSize: 20.0,fontWeight: FontWeight.w500),),
                   ),
                 ],
               ),
+              
 
 
             ],
@@ -77,6 +169,27 @@ class MyActivitiesState extends State<MyActivities>{
         ),
       ));
     }}
+
+
+
+  allClasses(){
+    return ListView.builder(
+      itemCount: workoutCards.length,
+      itemBuilder: (BuildContext context, int index){
+        return workoutCards[index];
+      }
+    );
+  }
+
+  allWorkouts(){
+    return ListView.builder(
+      itemCount: aulasFreqCards.length,
+      itemBuilder: (BuildContext context, int index){
+        return aulasFreqCards[index];
+      }
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +202,10 @@ class MyActivitiesState extends State<MyActivities>{
         {
 
           fillCardList(snapshot.data);
-          return new Scaffold(
+          return DefaultTabController(
+            length: 2,
+
+          child: new Scaffold(
           appBar: AppBar(
           title: Text("My Activities", style: TextStyle(color: Colors.white, fontSize: 25)),
           backgroundColor: Colors.deepOrangeAccent[200],
@@ -102,39 +218,32 @@ class MyActivitiesState extends State<MyActivities>{
                 }
             )
           ],
-          bottom: PreferredSize(
-            child: Row(children: <Widget>[
-              Container(
-                    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child:  Text("Class", style: TextStyle(color:  Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500), ),
-                  ),
+                bottom: TabBar(
 
-                  Container(
-                    margin: EdgeInsets.fromLTRB(100, 0, 0, 0),
-                    alignment: Alignment.centerRight,
-                    child: Text("Start", style: TextStyle(color:  Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),),
-                  ),
-                 Container(
-                    margin: EdgeInsets.fromLTRB(120, 0, 0, 0),
-                    alignment: Alignment.centerRight,
-                    child: Text("End", style: TextStyle(color: Colors.white, fontSize: 25.0, fontWeight: FontWeight.w500),),
-                  ),
-              ],
-            )
-            ,
-            preferredSize: Size.fromHeight(50)
+                  isScrollable: true,
+                  indicatorWeight: 2.0,
+                  indicatorColor: Colors.white,
+                  tabs: [
+                    Tab(child: Container(
+                      child: Text("CLASSES", style: TextStyle(color: Colors.white, fontSize: 16),),),
+                    ),
+                    Tab(child: Container(
+                      child: Text("WORKOUTS", style: TextStyle(color: Colors.white, fontSize: 16),),),
+                    ),
 
-            )
+                  ],
+                ),
           ),
           drawer: SideBar(nome: snapshot.data['nome'], numSocio: snapshot.data['numSocio'], img: snapshot.data['img'],),
 
-          body: ListView.builder(
-            itemCount: cardList.length,
-            itemBuilder: (BuildContext ctxt, int index){
-              return cardList[index];
-            },
+          body: TabBarView(
+            children:  [
+                  allWorkouts(),
+                  allClasses(),
+                ],
               ),
-            );
+            ),
+          );
         }
         else{
           return Loading();
@@ -143,4 +252,5 @@ class MyActivitiesState extends State<MyActivities>{
     );    
   }
 }
+
 
